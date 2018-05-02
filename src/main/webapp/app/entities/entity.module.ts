aa2ea092189292d5b68edbb7288d07a3ef137f33
edu <vizuete.eduardo@gmail.com>
2018-05-02 14:14:30 -0500

    create entity bloodpressure

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -2,12 +2,14 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { TwentyOnePointsPointsModule } from './points/points.module';
 import { TwentyOnePointsWeightModule } from './weight/weight.module';
+import { TwentyOnePointsBloodPressureModule } from './blood-pressure/blood-pressure.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
         TwentyOnePointsPointsModule,
         TwentyOnePointsWeightModule,
+        TwentyOnePointsBloodPressureModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
